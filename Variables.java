f1a6820e2893048b48ab8fa5e804adbfc47010cd
Abhishek kumar <abhishekkr8399@gmail.com>
2023-04-10 21:40:50 +0530

    java program

---
+++ Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John";
+        System.out.println(name);
+        int myNum = 15;
+        System.out.println(myNum);
+        float myFloatNum = 5.99f;
+        System.out.println(myFloatNum);
+        char myLetter = 'D';
+        System.out.println(myLetter);
+        boolean myBool = true;
+        System.out.println(myBool);
+        String myText = "Hello";
+        System.out.println(myText);
+	}
+
+}
